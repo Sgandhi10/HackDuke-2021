d2161b9013d048c98ef759325028c0efe3bb552f
Forrest M <forrestm@vt.edu>
2021-10-24 01:56:17 -0400

    Make Question Generator

--- Assets/Darts/MainScript.cs
+++ Assets/Darts/MainScript.cs
@@ -6,22 +6,17 @@ using TMPro;
 
 public class MainScript : MonoBehaviour
 {
-    List<Question> questions;
-    int currIndex = 0;
-    public Question CurrentQuestion { get => questions[currIndex]; }
+    QuestionDict questions;
+    public Question CurrentQuestion { get => questions.LastQuestion; }
     public GameObject Dart;
     public bool answered = false;
+    private int score = 0;
     // Start is called before the first frame update
     void Start()
     {
-        questions = new List<Question>();
-        currIndex = 0;
-        questions.Add(new Question("Chemical formula for sodium hydroixide?", new string[] { "SoHo", "NaBr", "LiOH", "NaOH" }, 3));
-        questions.Add(new Question("Chemical formula for bicarbonate ion?", new string[] { "HCO3 1-", "HClO4", "H2O", "HCO2 1-" }, 0));
-        questions.Add(new Question("Chemical formula for hydrochloric acid?", new string[] { "HBr", "HCl", "H2SO4", "HCl2" }, 1));
-
-        questions.Add(new Question("What is the oxidation number of sulfur in sulfate?", new string[] { "+2", "-4", "+6", "-8" }, 2));
+        questions = new QuestionDict();
         Instantiate(Dart, transform.position, Quaternion.identity, this.transform);
+        questions.GenerateQuestion();
         updateText();
     }
 
@@ -33,41 +28,39 @@ public class MainScript : MonoBehaviour
     public void onAnswer(int a)
     {
 
-        if (currIndex <= questions.Count && questions[currIndex].Answer != a)
+        if (CurrentQuestion.Answer != a)
         {
             // WRONG!!!
-            if(!answered){
+            if (!answered)
+            {
                 Debug.Log("Cringe!");
                 this.GetComponentsInChildren<Board>()[a].GetComponents<Board>()[0].SetColor(Color.red);
                 StartCoroutine(ExampleCoroutine(a, 1));
             }
-            
+
 
         }
         else
         {
-            currIndex++;
             answered = true;
-            if (currIndex == questions.Count)
-            {
-                // DONE!!!
-                this.GetComponentsInChildren<TextMeshPro>()[0].text = "u r smat";
-                for(int i = 0; i<4; i++){
-                    this.GetComponentsInChildren<Board>()[a].GetComponents<Board>()[0].SetColor(Color.green);}
-            }
-            else
-            {
-                Debug.Log("Hit target!" + a);
-                Debug.Log("Hit target!" + a);
-                Debug.Log("Hit target!" + a);
-                Debug.Log("Hit target!" + a);
-                Debug.Log("Hit target!" + a);
+            questions.GenerateQuestion();
+            // if (currIndex == questions.Count)
+            // {
+            //     // DONE!!!
+            //     this.GetComponentsInChildren<TextMeshPro>()[0].text = "u r smat";
+            //     for (int i = 0; i < 4; i++)
+            //     {
+            //         this.GetComponentsInChildren<Board>()[a].GetComponents<Board>()[0].SetColor(Color.green);
+            //     }
+            // }
+            // else
+            // {
                 Debug.Log("Hit target!" + a);
                 this.GetComponentsInChildren<Board>()[a].GetComponents<Board>()[0].SetColor(Color.green);
-                this.GetComponentInChildren<Canvas>().GetComponent<Text>().text = "Score: " + (currIndex);
+                this.GetComponentInChildren<Canvas>().GetComponent<Text>().text = "Score: " + score++;
                 StartCoroutine(ExampleCoroutine(a, 2));
 
-            }
+            // }
         }
     }
     public void updateText()
